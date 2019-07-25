0d8f257f61c855deb6a2aacc65f54ceff82e98d6
Gorden Biemüller <Gorden.biem@Googlemail.com>
2019-07-25 13:25:11 +0200

    13:25

--- WebContent/WEB-INF/view/main-menu.jsp
+++ WebContent/WEB-INF/view/main-menu.jsp
@@ -5,8 +5,9 @@
 
 
 <h2>Spring MVC Demo - Homepage</h2>
+<hr>
 
-
+<a href="showForm">Hello World form</a>
 
 </body>
 
--- src/de/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/de/luv2code/springdemo/mvc/HelloWorldController.java
@@ -1,6 +1,9 @@
 package de.luv2code.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,5 +18,23 @@ public class HelloWorldController {
 	@RequestMapping("/processForm")
 	public String processForm(){
 		return "helloworld";
+	}
+	@RequestMapping("/processFormVersionTwo")
+	public String letzShoutDude(HttpServletRequest request , Model model) {
+		
+		// read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+	
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result ="Yo! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+		
 	}
 }

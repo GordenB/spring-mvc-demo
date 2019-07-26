92a50830c37ff46980bba73cc9f19e53fbc0b444
Gorden Biemüller <Gorden.biem@Googlemail.com>
2019-07-26 08:57:33 +0200

    9:00

--- WebContent/WEB-INF/view/main-menu.jsp
+++ WebContent/WEB-INF/view/main-menu.jsp
@@ -6,8 +6,8 @@
 
 <h2>Spring MVC Demo - Homepage</h2>
 <hr>
-
-<a href="showForm">Hello World form</a>
+<a href="/showForm">Silly World form</a>
+<a href="hello/showForm">Hello World form</a>
 
 </body>
 
--- src/de/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/de/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	// need a controller method to show the initial HTML form
@@ -32,6 +34,26 @@ public class HelloWorldController {
 		
 		// create the message
 		String result ="Yo! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+		
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String theName,
+										Model model) {
+		
+		// read the request parameter from the HTML form // NO NEED
+		
+	
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result ="Hey My Friend from v3! " + theName;
 		
 		// add message to the model
 		model.addAttribute("message", result);

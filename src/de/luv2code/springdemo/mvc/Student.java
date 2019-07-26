6daee19293c44296be98304f41f7f1a355bac0a6
Gorden Biemüller <Gorden.biem@Googlemail.com>
2019-07-26 15:18:50 +0200

    Studenten Attribute hinzugefügt (Country,FavoriteLanguage,OperatingSystem)

--- src/de/luv2code/springdemo/mvc/Student.java
+++ src/de/luv2code/springdemo/mvc/Student.java
@@ -1,13 +1,28 @@
 package de.luv2code.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
+
 public class Student {
 
 	private String firstName;
 	private String lastName;
 	private String country;
+
+	private LinkedHashMap<String, String> countryOptions;
 	
+	private String favoriteLanguage;
+	private String[] operatingSystems;
+
 	public Student() {
-		
+		// populate country options: used ISO country code
+		countryOptions = new LinkedHashMap<>();
+
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+		countryOptions.put("JAVA", "Java");
 	}
 
 	public String getFirstName() {
@@ -34,7 +49,27 @@ public class Student {
 		this.country = country;
 	}
 
-	
-	
-	
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
+	}
+
+
+
+
 }
